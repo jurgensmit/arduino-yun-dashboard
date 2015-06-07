620da63506bcc525b71185078b5b192e14e564d2
Jurgen Smit <jurgen.smit@gmail.com>
2015-06-07 23:54:39 +0200

    added gulp

---
+++ gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    var config = {
+
+        // all js to vet
+        alljs: [
+            './AtherosAR9331/public/**/*.js',
+            './*.js'
+        ]
+    };
+
+    return config;
+};
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var args = require('yargs').argv;
+var config = require('./gulp.config')();
+
+var $ = require('gulp-load-plugins')({lazy: true});
+
+// var jshint = require('gulp-jshint');
+// var jscs = require('gulp-jscs');
+// var util = require('gulp-util');
+// var gulpprint = require('gulp-print');
+// var gulpif = require('gulp-if');
+
+gulp.task('vet', function() {
+    log('Analyzing source code with JSHint and JSCS');
+    return gulp
+		.src(config.alljs)
+		.pipe($.if(args.verbose, $.print()))
+		.pipe($.jscs())
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+		.pipe($.jshint.reporter('fail'));
+});
+
+function log(message) {
+    if (typeof message === 'object') {
+        for (var item in message) {
+            if (message.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(message[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(message));
+    }
+}
