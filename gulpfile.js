5ab52790439bb1e07df40d4cfe24f0035b4c5144
Jurgen Smit <jurgen.smit@gmail.com>
2015-06-08 08:24:04 +0200

    formatting

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,12 @@ var $ = require('gulp-load-plugins')({lazy: true});
 gulp.task('vet', function() {
     log('Analyzing source code with JSHint and JSCS');
     return gulp
-		.src(config.alljs)
-		.pipe($.if(args.verbose, $.print()))
-		.pipe($.jscs())
-		.pipe($.jshint())
-		.pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
-		.pipe($.jshint.reporter('fail'));
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
 
 function log(message) {
